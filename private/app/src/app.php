cccea08c0ebb51b2f003ffe0f1316b8f0d535a6e
francois joelle <francois.joelle1@gmail.com>
2019-02-19 14:27:01 +0100

    modif 3

---
+++ private/app/src/app.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../private/app/config/config.php";
+require_once "../private/app/src/environnement.php";
+require_once "../private/app/src/err_reporting.php";
+require_once "../private/app/src/db_connect.php";
+require_once "../private/app/src/routing.php";
+require_once "../private/app/src/compile.php";
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Web pizza</title>
-</head>
-<body>
-    <h1>webpizza</h1>
-</body>
-</html>
+<?php
+/**
+ * Fichier de démarrage de l'application
+ */
+
+// 1. Intégration du fichier d'exécution de l'application
+require_once "../private/app/src/app.php";
