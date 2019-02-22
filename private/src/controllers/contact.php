9c55951b9ce98c00c81f9a3142fef8df2d2269ac
francois joelle <francois.joelle1@gmail.com>
2019-02-22 16:35:46 +0100

    -

--- private/src/controllers/contact.php
+++ private/src/controllers/contact.php
@@ -8,9 +8,37 @@
  */
 function contact_index() 
 {
-    // Code 
-    // ...
+    if ($_SERVER['REQUEST_METHOD'] === "POST")
+    {
+        // Récupération des données
+        $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : null;
+        $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : null;
+        $email = isset($_POST['email']) ? $_POST['email'] : null;
+        $message = isset($_POST['message']) ? $_POST['message'] : null;
     
-    // Intégration de la vue
-    include_once "../private/src/views/contact/index.php";
+        // -- Contrôle des champs
+
+        // Contrôle du champ Prénom
+        
+        // Contrôle du champ nom
+        
+        // Contrôle du champ email
+
+        // Contrôle du champ message
+
+
+
+        dump( $_POST );
+        dump( $firstname );
+        dump( $lastname );
+        dump( $email );
+        dump( $message );
+
+    }
+
+    else {
+        // TODO: Suppression du else + redirection de l'utilisateur
+        echo "Le formulaire ne peut être traité qu'avec la méthode POST";
+    }
+
 }
